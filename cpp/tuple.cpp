e5019bff4fd2464fbc2905da110e70bba20143b5
Griffin Melnick <melnick.griffin@gmail.com>
2018-09-07 08:50:04 -0400

    Playing with tuples
    Specifically looking to see if one-tuples are valid.

---
+++ cpp/tuple.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <tuple>
+
+int main() {
+    auto t = std::make_tuple( 3, 4 );
+    std::cout << '(' << std::get<0>(t) << ", " << std::get<1>(t) << ')' << std::endl;
+}
